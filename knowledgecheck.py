74062149e7a05ca85c0d22aeacf99137b010e3e4
Mayowa-source <titiloye.mayowa@gmail.com>
2022-09-27 20:16:14 -0400

    third commit

--- knowledgecheck.py
+++ knowledgecheck.py
@@ -1,3 +1,17 @@
+#PrintStatement
 Greetings = 'Hello World!'
 print(Greetings)
 
+#list
+tm = ['12:00 pm', 1.3, 1, 'Holiday']
+print(tm[3])
+
+#dictionary
+Address = {
+    'Number': 336, 
+    'Street': 'Scavanger road'}
+print(Address['Street'])
+
+#tuple
+tm_tuple = ('terry', 'Mason', '365', 'Walker')
+print(tm_tuple[2])
